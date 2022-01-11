18559b62aac5df8956e13b08985b85a4cc3a0f26
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2022-01-11 12:05:52 +0600

    all done

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,16 @@
 const User = require("../model/User");
 
+//create user
+exports.createUser = async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ message: err });
+    }
+};
+
 //get all users
 exports.getAllUsers = async (req, res) => {
     try {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,10 +4,11 @@ const {
     getUserById,
     updateUser,
     deleteUser,
+    createUser,
 } = require("../controller/userController");
 const router = express.Router();
 
-router.route("/").get(getAllUsers);
+router.route("/").get(getAllUsers).post(createUser);
 router.route("/:id").get(getUserById).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
