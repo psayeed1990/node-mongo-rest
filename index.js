4449357b621cd971827df1bff43a2159993d656e
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2022-01-11 10:54:47 +0600

    started server

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+
+// Load env variables
+dotenv.config();
+
+//cors
+app.use(cors());
+
+//connect mongoose
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+//urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//accept json
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
